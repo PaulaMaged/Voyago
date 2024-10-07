2d933a19b1416430fdb48634ecb9bfd4c429327d
Omar Eladly <omar.eladly.dmet@gmail.com>
2024-10-07 21:23:29 +0300

    added Tour Guide

--- backend/controllers/TourGuideController.js
+++ backend/controllers/TourGuideController.js
@@ -1,5 +1,57 @@
 import Itinerary from "../models/Itinerary.js";
 import Booking from "../models/Booking.js";
+import TourGuide from "../models/TourGuide.js";
+
+//create Tour Guide 
+const createTourGuide = async (req, res) => {
+  try {
+    const newTourGuide = new TourGuide(req.body);
+    const savedTourGuide = await newTourGuide.save();
+    res.status(201).json(savedTourGuide);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//get Tour Guide by id 
+const getTourGuideById = async (req, res) => {
+  try {
+    const tourGuide = await TourGuide.findById(req.params.id);
+    if (!tourGuide) return res.status(404).json({ message: "Tour Guide not found" });
+    res.status(200).json(tourGuide);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//update Tour Guide by id
+const updateTourGuideById = async (req, res) => {
+  try {
+    const updatedTourGuide = await TourGuide.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    if (!updatedTourGuide)
+      return res.status(404).json({ message: "Tour Guide not found" });
+    res.status(200).json(updatedTourGuide);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//delete Tour Guide by id
+const deleteTourGuideById = async (req, res) => {
+  try {
+    const deletedTourGuide = await TourGuide.findByIdAndDelete(req.params.id);
+    if (!deletedTourGuide)
+      return res.status(404).json({ message: "Tour Guide not found" });
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 
 // Create an Itinerary
 const createItinerary = async (req, res) => {
@@ -77,4 +129,8 @@ export default {
   updateItinerary,
   deleteItinerary,
   getTourGuideItineraries,
+  createTourGuide,
+  getTourGuideById,
+  updateTourGuideById,
+  deleteTourGuideById,
 };
