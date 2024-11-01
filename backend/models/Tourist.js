c20fe72a64e537258c0747c9e12b95c0e6fd432c
numberw8835 <numberw8835@gmail.com>
2024-11-01 15:08:16 +0200

    Added necessary attributes to complete my stuff

--- backend/models/Tourist.js
+++ backend/models/Tourist.js
@@ -3,6 +3,22 @@ import User from "./User.js";
 
 const touristSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  points: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  level: {
+    type: Number,
+    default: 1,
+    min: 1,
+    max: 3,
+  },
+  badges: [
+    {
+      type: String,
+    },
+  ],
   phone_number: { type: String },
   nationality: { type: String },
   is_student: { type: Boolean, default: false },
