99f365ab0408e810d0a426b7638c63aa8e1a781f
numberw8835 <numberw8835@gmail.com>
2024-11-04 11:58:12 +0200

    New models

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -4,19 +4,38 @@ import Tourist from "./Tourist.js";
 import Itinerary from "./Itinerary.js";
 import Location from "./Location.js";
 
-const bookingSchema = new mongoose.Schema({
-  activity: { type: mongoose.Schema.Types.ObjectId, ref: "Activity" },
-  tourist: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Tourist",
-    required: true,
+const bookingSchema = new mongoose.Schema(
+  {
+    plan_type: {
+      type: String,
+      enum: ["Activity", "Itinerary"],
+      required: true,
+    },
+    plan_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    tourist: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tourist",
+      required: true,
+    },
+    attended: {
+      type: Boolean,
+      default: false,
+    },
+    booking_date: {
+      type: Date,
+      default: Date.now,
+    },
+    booking_time: {
+      type: String,
+    },
   },
-  itinerary: { type: mongoose.Schema.Types.ObjectId, ref: "Itinerary" },
-  attended: { type: Boolean },
-  booking_date: { type: Date, default: Date.now },
-  booking_time: { type: String },
-  location: { type: mongoose.Schema.Types.ObjectId, ref: "Location" },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
---
+++ backend/models/Complaint.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+import Itinerary from "./Itinerary.js";
+import Activity from "./Activity.js";
+
+const complaintSchema = new Schema({
+  subject_of_complaint: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  subject_type: {
+    type: String,
+    enum: ["Itinerary", "Activity"],
+    required: true,
+  },
+  reviewer: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  state: {
+    type: String,
+    enum: ["pending", "resolved"],
+    default: "pending",
+  },
+  date: { type: Date, default: Date.now },
+});
+
+const Complaint = mongoose.model("Complaint", complaintSchema);
+export default Complaint;
