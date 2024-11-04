006beed108e61528fd243a8578d9b59109e07276
numberw8835 <numberw8835@gmail.com>
2024-11-04 23:14:37 +0200

    Todo

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# Stuff that should be done
+
+- [ ] tourGuide_get_profile_info
+- [ ] Tourist_get_balance
+  - [ ] Update_profile
+- [ ] check if the account doesn't have same email, before creation
+- [ ] Add email field to USER, except admin
+- [ ] Edit the adv4ertizer to make sure their is a booking be ---> id:19 give to omer
+- [ ] Problem in tags
+- [ ] What is an itinerary --> omer
+- [ ] add price to LAndMArk
+- [ ] How to create tags while creating the activity like youtube
+- [ ] Ask the prof for an example of an itinerary --> Omer
--- backend/controllers/SellerController.js
+++ backend/controllers/SellerController.js
@@ -1,5 +1,5 @@
-import Product from '../models/Product.js'
-import Seller from '../models/Seller.js'
+import Product from "../models/Product.js";
+import Seller from "../models/Seller.js";
 
 /**
  * Creates a new seller in the database.
@@ -10,17 +10,16 @@ import Seller from '../models/Seller.js'
  * @returns {Object} - The created seller object or an error message.
  * @throws Will throw an error if the seller data is invalid or if there's a database error.
  */
-const createSeller = async (req, res, ) => {
-    const seller = req.body;
-    try {
-        const newSeller = new Seller(seller);
-        const savedSeller = await newSeller.save();
-        res.status(201).json(savedSeller);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
-
+const createSeller = async (req, res) => {
+  const seller = req.body;
+  try {
+    const newSeller = new Seller(seller);
+    const savedSeller = await newSeller.save();
+    res.status(201).json(savedSeller);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 /**
 
@@ -33,13 +32,15 @@ const createSeller = async (req, res, ) => {
  * @returns {Object} - A JSON response containing an array of sellers or an error message.
  */
 const getSellersByUserId = async (req, res) => {
-    try {
-        const sellers = await Seller.find({ user: req.params.userId }).populate('user');
-        res.json(sellers);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const sellers = await Seller.find({ user: req.params.userId }).populate(
+      "user"
+    );
+    res.json(sellers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 /**
  * Retrieves all products associated with a specific seller ID.
@@ -51,93 +52,105 @@ const getSellersByUserId = async (req, res) => {
  * @returns {Object} - A JSON response containing an array of products or an error message.
  */
 const getProductsBelongingToSeller = async (req, res) => {
-    try {
-        const products = await Product.find({ seller: req.params.sellerId });
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const products = await Product.find({ seller: req.params.sellerId });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const updateSellerById = async (req, res) => {
-    try {
-        const updatedSeller = await Seller.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedSeller) return res.status(404).json({ message: 'Seller not found' });
-        res.json(updatedSeller);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const updatedSeller = await Seller.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedSeller)
+      return res.status(404).json({ message: "Seller not found" });
+    res.json(updatedSeller);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const deleteSellerById = async (req, res) => {
-    try {
-        const deletedSeller = await Seller.findByIdAndDelete(req.params.id);
-        if (!deletedSeller) return res.status(404).json({ message: 'Seller not found' });
-        res.json({ message: 'Seller deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const deletedSeller = await Seller.findByIdAndDelete(req.params.id);
+    if (!deletedSeller)
+      return res.status(404).json({ message: "Seller not found" });
+    res.json({ message: "Seller deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const createProduct = async (req, res) => {
-    const product = req.body;
-    try {
-        const newProduct = new Product(product);
-        const savedProduct = await newProduct.save();
-        res.status(201).json(savedProduct);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  const product = req.body;
+  try {
+    const newProduct = new Product(product);
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const getProductById = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) return res.status(404).json({ message: 'Product not found' });
-        res.json(product);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const getAllProducts = async (req, res) => { 
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
-
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const updateProductById = async (req, res) => {
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedProduct) return res.status(404).json({ message: 'Product not found' });
-        res.json(updatedProduct);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedProduct)
+      return res.status(404).json({ message: "Product not found" });
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const deleteProductById = async (req, res) => {   
+const deleteProductById = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct)
+      return res.status(404).json({ message: "Product not found" });
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    try {
-        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-        if (!deletedProduct) return res.status(404).json({ message: 'Product not found' });
-        res.json({ message: 'Product deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
-
-export default {createProduct, 
-    getAllProducts, 
-    getProductById , 
-    deleteProductById, 
-    updateProductById,
-    getSellersByUserId,
-    createSeller,
-    updateSellerById,
-    deleteSellerById,
-    getProductsBelongingToSeller}
+export default {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+  updateProductById,
+  getSellersByUserId,
+  createSeller,
+  updateSellerById,
+  deleteSellerById,
+  getProductsBelongingToSeller,
+};
