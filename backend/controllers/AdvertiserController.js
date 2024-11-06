85dfd564bc67a9917a229cddd688ea410fb5ce82
numberw8835 <numberw8835@gmail.com>
2024-11-06 17:12:19 +0200

    Added the thing Hossam wanted

--- backend/controllers/AdvertiserController.js
+++ backend/controllers/AdvertiserController.js
@@ -154,7 +154,7 @@ const getAdvertiserActivities = async (req, res) => {
 
 const getAllActivities = async (req, res) => {
   try {
-    const activities = await Activity.find();
+    const activities = await Activity.find().populate("advertiser");
     res.status(200).json(activities);
   } catch (error) {
     res.status(500).json({ error: error.message });
