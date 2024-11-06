8d8480b55a02382cbb18ca045a45b424da17114f
numberw8835 <numberw8835@gmail.com>
2024-11-06 20:45:01 +0200

    changed the names in publicRoutes

--- backend/routes/PublicRoutes.js
+++ backend/routes/PublicRoutes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 import PublicController from "../controllers/PublicController.js";
 
 // View All Upcoming Activities
-router.get("/activities/upcoming", PublicController.getUpcomingActivities);
+router.get("/get-upcoming-events", PublicController.getUpcomingActivities);
 
 // View All Itineraries
-router.get("/itineraries", PublicController.getAllItineraries);
+router.get("get-all-itineraries", PublicController.getAllItineraries);
 
 // View All Historical Places/Museums
-router.get("/landmarks", PublicController.getAllLandmarks);
+router.get("/get-all-landmarks", PublicController.getAllLandmarks);
 
 export default router;
