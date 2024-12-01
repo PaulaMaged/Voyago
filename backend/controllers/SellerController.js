7976a9305942081f4de3027cb37aeb672f7b9975
numberw8835 <numberw8835@gmail.com>
2024-12-01 19:54:31 +0200

    fixed bug

--- backend/controllers/SellerController.js
+++ backend/controllers/SellerController.js
@@ -328,7 +328,7 @@ const getSalesReportFiltered = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-
+};
 
 export default {
   createProduct,
