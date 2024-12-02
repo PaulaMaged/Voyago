7168daac0e3bcf093c970ca62db348378f8f78bf
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2024-12-02 20:33:28 +0200

    removed unused mongoose import in ShowcasePromoCodes as it causes an error

--- voyago_ui/src/components/Admin_Dashboard/ShowcasePromoCodes.jsx
+++ voyago_ui/src/components/Admin_Dashboard/ShowcasePromoCodes.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { set } from "mongoose";
 
 // This is hardcoded data for demonstration purposes
 const hardcodedPromoCodes = [
